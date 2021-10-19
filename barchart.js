994ba39709308d5621aa40837599604e453a07d7
IT62070227 <50777893+imlanzelotx@users.noreply.github.com>
2021-10-19 10:41:56 +0700

    Update

--- barchart.js
+++ barchart.js
@@ -1,29 +1,27 @@
-var ctx1 = document.getElementById("myChartbar").getContext('2d');
-var chart = new Chart(ctx1, {    
+var bar = document.getElementById("chartBar").getContext('2d');
+var chartBar = new Chart(bar, {    
     type: 'bar',
     data: {
         barPercentage: 100,
         labels: ['2559', '2560', '2561', '2562', '2563'],
-        datasets: [{
+        datasets: [ {
         label:'เพศหญิง',
         data:[61.6, 59.9, 60.5, 58.7, 60.2],
         backgroundColor:'#ed3f33',
-        
-    },{
+        }, {
         label:'เพศชาย',
         data:[77.9, 77.0, 77.6, 76.0, 76.6],
         backgroundColor:'#183965'
-    }]
-},
-option: {
-    scales: {
-        x: {
-            grid: {
-              offset: true
+        } ]
+    },
+
+    option: {
+        scales: {
+            x: {
+                grid: {
+                offset: true
+                }
             }
         }
     }
-
-    
-}
 });
--- piechart.js
+++ piechart.js
@@ -1,17 +1,21 @@
-let ctx = document.getElementById('myChart').getContext('2d');
+let doughnut = document.getElementById('chartDoughnut').getContext('2d');
 let labels = ['ค่าตอบแทน', 'งานขาดต่อเนื่อง', 'งานหนัก', 'ไม่มีสวัสดิการ', 'ทำงานไม่ตรงเวลา', 'ไม่มีวันหยุด', 'ชั่วโมงทำงานเยอะเกินไป', 'ลาหยุดหรือลาพักผ่อนไม่ได้']; 
 let colorHex = ['#f56061', '#ff8358', '#ffa02e', '#fbb815', '#ffda7d', '#00727d', '#023767', '#7c539d'];
 
-let myChart = new Chart(ctx, {
+let chartDoughnut = new Chart(doughnut, {
   type: 'doughnut',
   data: {
+    //  labels: labels,
     datasets: [{
       data: [58.1, 14.8, 11.5, 9, 2.8, 1.4, 1.4, 1],
       backgroundColor: colorHex
     }],
-    labels: labels
   },
+  
   option: {
     responsive: true
   }
 })
+
+chartDoughnut.canvas.parentNode.style.height = '100%'; 
+chartDoughnut.canvas.parentNode.style.width = '30vw';
