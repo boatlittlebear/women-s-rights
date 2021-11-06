aafcb57fe91ad6a8997e5f060547c5f54fd83f34
Prakorn2033 <56525189+Prakorn2033@users.noreply.github.com>
2021-11-07 00:58:21 +0700

    ใส่ ป็อปอัพทวิต (อยากได้ขนาดหน้าเพิ่ม)
    เหลือข้อคว่มในทวิตอีก 5 ข้อความครับ

---
+++ button.js
@@ -0,0 +1,36 @@
+const open1 = document.getElementById('open1');
+const modal_container1 = document.getElementById('modal_container1');
+const close1 = document.getElementById('close1');
+
+open1.addEventListener('click',() => {
+    modal_container1.classList.add('show')
+});
+
+close1.addEventListener('click',() => {
+    modal_container1.classList.remove('show')
+});
+
+
+const open2 = document.getElementById('open2');
+const modal_container2 = document.getElementById('modal_container2');
+const close2 = document.getElementById('close2');
+
+open2.addEventListener('click',() => {
+    modal_container2.classList.add('show')
+});
+
+close2.addEventListener('click',() => {
+    modal_container2.classList.remove('show')
+});
+
+const open3 = document.getElementById('open3');
+const modal_container3 = document.getElementById('modal_container3');
+const close3 = document.getElementById('close3');
+
+open3.addEventListener('click',() => {
+    modal_container3.classList.add('show')
+});
+
+close3.addEventListener('click',() => {
+    modal_container3.classList.remove('show')
+});
