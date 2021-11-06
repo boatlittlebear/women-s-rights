db8c4f3abe4488b3e3f6a0b812d634de5af23caf
Prakorn2033 <56525189+Prakorn2033@users.noreply.github.com>
2021-11-07 03:13:51 +0700

    ทำทวิตเสร็จแล้วเหลือ(เหลือเปลื่ยนรูปกับดึงหัวข้อลงมา)

--- button.js
+++ button.js
@@ -34,3 +34,42 @@ open3.addEventListener('click',() => {
 close3.addEventListener('click',() => {
     modal_container3.classList.remove('show')
 });
+
+
+const open4 = document.getElementById('open4');
+const modal_container4 = document.getElementById('modal_container4');
+const close4 = document.getElementById('close4');
+
+open4.addEventListener('click',() => {
+    modal_container4.classList.add('show')
+});
+
+close4.addEventListener('click',() => {
+    modal_container4.classList.remove('show')
+});
+
+
+const open5 = document.getElementById('open5');
+const modal_container5 = document.getElementById('modal_container5');
+const close5 = document.getElementById('close5');
+
+open5.addEventListener('click',() => {
+    modal_container5.classList.add('show')
+});
+
+close5.addEventListener('click',() => {
+    modal_container5.classList.remove('show')
+});
+
+
+const open6 = document.getElementById('open6');
+const modal_container6 = document.getElementById('modal_container6');
+const close6 = document.getElementById('close6');
+
+open6.addEventListener('click',() => {
+    modal_container6.classList.add('show')
+});
+
+close6.addEventListener('click',() => {
+    modal_container6.classList.remove('show')
+});
