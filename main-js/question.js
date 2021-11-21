20ed546538d39dae50911abd945be126b92c7ccf
IT62070227 <50777893+imlanzelotx@users.noreply.github.com>
2021-11-22 01:06:21 +0700

    Save

--- main-js/question.js
+++ main-js/question.js
@@ -28,16 +28,9 @@ let questions = [
     }
 ];
 
-let description = [
-    "เพิ่มความนับถือตัวเองของคุณของคุณ แสดงถึงความเข้มแข็ง และกล้าที่จะแสดงออก คุณค่าของเสียงและความคิดเห็น ช่วยสนับสนุนผู้หญิงที่อยู่รอบข้างคุณ ไม่กลัวที่จะใช้เสียงและความสามารถของพวกเขา และอย่าให้คำพูดโอ้อวดหรือคำดูถูกมากดกินความมั่นใจของเราไป",
-    "ต่อสู้กับการปฏิเสธ ต่อสู้กับมาตราฐานของสังคมที่ลดทอนสิทธิ์ของสตรี ไม่ว่าจะเป็น มาตราฐานความสวย ความเหมาะสมในการทำงาน การเผชิญกับแง่ลบอยู่เสมอ ดังนั้นเผชิญหน้าและมองในแง่ดีว่ามีผู้หญิงอีกมากมายที่ลบล้างมาตราฐานเหล่านี้ได้เหมือนกัน",
-    "เลือกที่จะเป็นนักรบ ไม่ใช่เหยื่อ ไม่ว่าคุณจะรู้สึกท้อแท้หรือหมดหนทางและพบว่ารู้สึกตกเป็นเหยื่อของใครบางคน จงรับรู้ไว้ว่า คุณมีทางเลือกเสมอ ให้พร้อมเรียนรู้และเผชิญหน้าเหมือนนักรบ  เปลี่ยนความคิดและกรอบความคิดของคุณ คุณสามารถทำได้แน่นอน"
-]
-
 /* ==== True code ==== */
 const questionArea = document.querySelector(".questionArea");
 const scoreArea = document.querySelector(".scoreArea");
-const scoreText1 = document.querySelector(".scoreText1");
 const scorePct = document.querySelector(".scorePct");
 
 //initial data
@@ -45,15 +38,6 @@ let currentQuestion = 0;
 let correctAnswers = 0;
 
 showQuestion();
-
-//reset event
-document.querySelector(".scoreArea button").addEventListener("click", () => {
-  var url="https://empowered-women.netlify.app"; //Set desired URL here
-  var img="https://media.discordapp.net/attachments/883616927925297193/912022146019106846/Frame_92.jpg?width=384&height=683"; //Set Desired Image here
-  var totalurl=encodeURIComponent(url+'?img='+img);
-
-  window.open ('http://www.facebook.com/sharer.php?u='+totalurl,'','width=500, height=500, scrollbars=yes, resizable=no');
-});
 
 //Functions
 function showQuestion() {
@@ -97,12 +81,14 @@ function finishQuiz() {
   let points = correctAnswers;
   
   if (points >= 2) {
-    scorePct.dataset.image = "share1"
+    scorePct.dataset.image = "share1";
+    share.href = "/img/page-20/share-1.png";
   } else if (points >= 1) {
-    scorePct.dataset.image = "share2"
-
+    scorePct.dataset.image = "share2";
+    share.href = "/img/page-20/share-2.png";
   } else if (points == 0) {
-    scorePct.dataset.image = "share3"
+    scorePct.dataset.image = "share3";
+    share.href = "/img/page-20/share-3.png";
   }
   
   scoreArea.style.display = "flex";
