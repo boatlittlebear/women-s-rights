e40a800d272e95afce8801b1e980260854e5e724
IT62070227 <50777893+imlanzelotx@users.noreply.github.com>
2021-11-22 16:43:18 +0700

    Edit Js

--- game.js
+++ game.js
@@ -1,25 +1,19 @@
 let count = 0;
 
-function wrongClick()
-  {    
-    let property = document.getElementById(clicked_id);
-    property.style.backgroundColor = "#FF0000"
+function wrongClick(clicked_id) {    
+    var property = document.getElementById(clicked_id);
+    property.style.background = "#FF0000"
     count=1;
     if (count == 1){
         alert("ยังไม่ถูกลองอีกครั้งนะ")     
     }
-
-
 }
 
-function correctClick()
-  {
-    
-    let property = document.getElementById(clicked_id);
-    property.style.backgroundColor = "#32CD32"
+function correctClick(clicked_id) {
+    var property = document.getElementById(clicked_id);
+    property.style.background = "#32CD32"
     count = -1;
     if (count == -1){
         alert("ยินดีด้วยคุณตอบถูก")     
     }
-
   }
