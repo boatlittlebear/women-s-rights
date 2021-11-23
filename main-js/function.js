b98307aaf7d39c9ebda0beaeb12603415746be2a
IT62070227 <50777893+imlanzelotx@users.noreply.github.com>
2021-11-23 09:13:30 +0700

    Update btn Twt

--- main-js/function.js
+++ main-js/function.js
@@ -2,7 +2,7 @@ let scroller = document.querySelector("#scroller");
 const output = document.querySelector("#output");
 
 scroller.addEventListener("scroll", event => {
-  output.textContent = `scrollTop: ${scroller.scrollTop}`;
+  //output.textContent = `scrollTop: ${scroller.scrollTop}`;
 });
 
 scroller.addEventListener("scroll", event => {
