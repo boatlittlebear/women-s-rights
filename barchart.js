e3f3d472f8bfd847695e3eeace277e94de6c7dab
IT62070227 <50777893+imlanzelotx@users.noreply.github.com>
2021-12-10 00:08:24 +0700

    Edit

--- barchart.js
+++ barchart.js
@@ -5,13 +5,13 @@ var chartBar = new Chart(bar, {
         barPercentage: 100,
         labels: ['2559', '2560', '2561', '2562', '2563'],
         datasets: [ {
-        label:'เพศหญิง',
-        data:[61.6, 59.9, 60.5, 58.7, 60.2],
-        backgroundColor:'#ed3f33',
-        }, {
-        label:'เพศชาย',
-        data:[77.9, 77.0, 77.6, 76.0, 76.6],
-        backgroundColor:'#183965'
+            label:'เพศชาย',
+            data:[77.9, 77.0, 77.6, 76.0, 76.6],
+            backgroundColor:'#183965'
+            }, {
+            label:'เพศหญิง',
+            data:[61.6, 59.9, 60.5, 58.7, 60.2],
+            backgroundColor:'#ed3f33',
         } ]
     },
 
